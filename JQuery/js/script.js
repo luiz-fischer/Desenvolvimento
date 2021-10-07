f3fe5660d04e046bbf882893d3c1ef2221909a8f
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-10-07 19:16:44 -0300

    feat: ok

---
+++ JQuery/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    // $('section > div > div h1').hide();
+
+    // $('button').click(function() {
+    //     $('#spanHide').hide();
+    // });
+
+    $('span').html("<p>Mudei para novo texto</p>");
+    
+});
+
+
+
+
+
