d8eb4306173b6c9663a569bd941c6a370b1cf5b5
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-10-08 20:37:43 -0300

    feat: ok

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $("input[id!='primeiroNome']").attr('readonly', true)
-   
-  $('input').on('input propertychange', function () {
-      if ($(this).val() == "") {
-          $("input[id!='primeiroNome']").attr('readonly', true)
-      } else {
-          $("input").attr('readonly', false)
-      }
+  $("input[id!='primeiroNome']").attr("readonly", true);
+
+  $("input").on("input propertychange", function () {
+    if ($(this).val() == "") {
+      $("input[id!='primeiroNome']").attr("readonly", true);
+    } else {
+      $("input").attr("readonly", false);
+    }
   });
 });
