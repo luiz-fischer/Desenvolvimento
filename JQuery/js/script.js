000a72debbb4046a8642d959c3b2363007281789
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-10-14 19:07:42 -0300

    feat: ok

--- JQuery/js/script.js
+++ JQuery/js/script.js
@@ -1,11 +1,28 @@
-$(document).ready(function () {
-  $("input[id!='primeiroNome']").attr("readonly", true);
+// $(document).ready(function () {
+//   $("input[id!='primeiroNome']").attr("readonly", true);
 
-  $("input").on("input propertychange", function () {
-    if ($(this).val() == "") {
-      $("input[id!='primeiroNome']").attr("readonly", true);
-    } else {
-      $("input").attr("readonly", false);
-    }
+//   $("input").on("input propertychange", function () {
+//     if ($(this).val() == "") {
+//       $("input[id!='primeiroNome']").attr("readonly", true);
+//     } else {
+//       $("input").attr("readonly", false);
+//     }
+//   });
+// });
+
+$(document).ready(function () {
+  $(".teste").click(function () {
+    $("#teste").slideToggle();
   });
+  $(".teste2").click(function () {
+    $("#teste2").slideToggle();
+  });
+  $(".teste3").click(function () {
+    $("#teste3").slideToggle();
+  });
+});
+
+  $('#navbar').click(function(){
+    $(this).children('a').toggleClass('active');
+    $(this).siblings('li').children('a').removeClass('active');
 });
