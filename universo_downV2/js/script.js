818a7f49aeae110badbb6d6fccf39a4130c24074
luiz-fischer <luiz-fischer@hotmail.com>
2021-10-15 17:36:55 -0300

    feat: ok

--- universo_downV2/js/script.js
+++ universo_downV2/js/script.js
@@ -10,11 +10,4 @@ $(".main-container.collapse").on("shown.bs.collapse", function () {
 $.get("menu.html", function (data) {
   $("#nav-placeholder").replaceWith(data);
 });
-$(document).ready(function(){
-  $("#form-cadastro").click(function(){
-       
-      $(function(){
-          $("#form-cadastro-estrela").load("cadastro_estrela.html"); 
-      });
-  })
-});
+
